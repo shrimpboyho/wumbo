e402905e5cd23cdf63ca2c5bc9a92e32ef7dc1e2
shrimpydubsy <new.joseph10@gmail.com>
2013-08-24 14:43:42 -0400

    Update wumbo.h

--- wumbo.h
+++ wumbo.h
@@ -1,26 +1,22 @@
 #ifndef WUMBO_H
 #define WUMBO_H
 
-(function () {
-    /* Extend string */
-    String.prototype.replaceBetween = function (start, end, what) {
-        return this.substring(0, start) + what + this.substring(end);
-    };
+/* OPERATOR STRUCT */
 
-    /* Constructor */
-    window.Wumbo = function Wumbo() {
-        this.OPERATORS = {
-            add: '+',
-            sub: '-',
-            mul: '*',
-            div: '/',
-            exp: '^',
-            o_paren: '(',
-            c_paren: ')'
-        };
-    };
+typedef struct operators {
+    
+    char add = '+';
+    char sub = '-';
+    char mul = '*';
+    char div = '/';
+    char exp = '^';
+    char o_paren = '(';
+    char c_paren = ')';
+    
+} OPERATORS;
 
-    /* Main parser */
+/* MAIN RECURSIVE FUNCTION */
+
     Wumbo.prototype.parse = function (expression) {
         if (expression.indexOf(this.OPERATORS.o_paren) !== -1) {
             while (expression.indexOf(this.OPERATORS.o_paren) !== -1) {
@@ -76,101 +72,109 @@
         return expression;
     };
 
-    /* OPERATOR FUNCTIONS */
+/* OPERATOR FUNCTIONS */
 
-    Wumbo.prototype.add = function (a, b) {
-        return (parseInt(a, 10) + parseInt(b, 10)).toString();
-    };
+char* wum_add(char* a, char* b) {
+    
+    return (parseInt(a, 10) + parseInt(b, 10)).toString();
+    
+}
 
-    Wumbo.prototype.sub = function (a, b) {
-        return (parseInt(a, 10) - parseInt(b, 10)).toString();
-    };
+char* wum_sub(char* a, char* b) {
+    
+    return (parseInt(a, 10) - parseInt(b, 10)).toString();
+    
+}
 
-    Wumbo.prototype.mul = function (a, b) {
-        return (parseInt(a, 10) * parseInt(b, 10)).toString();
-    };
+char* wum_mul(char* a, char* b) {
+    
+    return (parseInt(a, 10) * parseInt(b, 10)).toString();
+    
+}
 
-    Wumbo.prototype.div = function (a, b) {
-        return (parseInt(a, 10) / parseInt(b, 10)).toString();
-    };
+char* wum_div(char* a, char* b) {
+    
+    return (parseInt(a, 10) / parseInt(b, 10)).toString();
+    
+}
 
-    Wumbo.prototype.exp = function (a, b) {
-        return (Math.pow(parseInt(a, 10), parseInt(b, 10))).toString();
-    };
+char* wum_exp(char* a, char* b) {
+ 
+    return (Math.pow(parseInt(a, 10), parseInt(b, 10))).toString();
 
-    /* LOGICAL FUNCTIONS */
+}
 
-    Wumbo.prototype.isOp = function (k) {
-        if (k === this.OPERATORS.add
-            || k === this.OPERATORS.sub
-            || k === this.OPERATORS.mul
-            || k === this.OPERATORS.div
-            || k === this.OPERATORS.exp
-            || k === this.OPERATORS.o_paren
-            || k === this.OPERATORS.c_paren) {
-            return true;
-        } else {
-            return false;
+/* LOGICAL FUNCTIONS */
+
+bool isOp(char k){
+    if (k == OPERATORS.add
+        || k == OPERATORS.sub
+        || k == OPERATORS.mul
+        || k == OPERATORS.div
+        || k == OPERATORS.exp
+        || k == OPERATORS.o_paren
+        || k == OPERATORS.c_paren) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Mainipulation functions */
+char* simplify(char* expression, int i) {
+    int k, j;
+    char opsign = expression[i];
+
+    // determine left operand index
+    for (k = i - 1; k >= 0; k--) {
+        if (this.isOp(expression[k])) {
+            break;
         }
-    };
+    }
+    var leftstart = k + 1;
 
-    /* Mainipulation functions */
-    Wumbo.prototype.simplify = function (expression, i) {
-        var k, j;
-        var opsign = expression[i];
+    // determine right operand index
+    for (j = i + 1; j < expression.length; j++) {
+        if (this.isOp(expression[j])) {
+            break;
+        }
+    }
+    var rightstart = j - 1;
+    console.log('left: ' + leftstart + ' right: ' + rightstart);
 
-        // determine left operand index
-        for (k = i - 1; k >= 0; k--) {
-            if (this.isOp(expression[k])) {
-                break;
-            }
-        }
-        var leftstart = k + 1;
+    // Do splicing, simplification, and replacement
+    var op1 = expression.slice(leftstart, i).trim();
+    var op2 = expression.slice(i + 1, rightstart + 1).trim();
+    console.log("Left hand operand: " + op1);
+    console.log("Right hand operand: " + op2);
 
-        // determine right operand index
-        for (j = i + 1; j < expression.length; j++) {
-            if (this.isOp(expression[j])) {
-                break;
-            }
-        }
-        var rightstart = j - 1;
-        console.log('left: ' + leftstart + ' right: ' + rightstart);
-
-        // Do splicing, simplification, and replacement
-        var op1 = expression.slice(leftstart, i).trim();
-        var op2 = expression.slice(i + 1, rightstart + 1).trim();
-        console.log("Left hand operand: " + op1);
-        console.log("Right hand operand: " + op2);
-
-        if (opsign === this.OPERATORS.add) {
-            var stuffing = this.add(op1, op2);
-            expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
-            console.log("SIMPLIFICAITON: " + expression);
-        }
-        if (opsign === this.OPERATORS.sub) {
-            var stuffing = this.sub(op1, op2);
-            expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
-            console.log("SIMPLIFICAITON: " + expression);
-        }
-        if (opsign === this.OPERATORS.mul) {
-            var stuffing = this.mul(op1, op2);
-            expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
-            console.log("SIMPLIFICAITON: " + expression);
-        }
-        if (opsign === this.OPERATORS.div) {
-            var stuffing = this.div(op1, op2);
-            expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
-            console.log("SIMPLIFICAITON: " + expression);
-        }
-        if (opsign === this.OPERATORS.exp) {
-            var stuffing = this.exp(op1, op2);
-            expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
-            console.log("SIMPLIFICAITON: " + expression);
-        }
-
-        return expression;
+    if (opsign == this.OPERATORS.add) {
+        var stuffing = this.add(op1, op2);
+        expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
+        console.log("SIMPLIFICAITON: " + expression);
+    }
+    if (opsign == OPERATORS.sub) {
+        var stuffing = this.sub(op1, op2);
+        expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
+        console.log("SIMPLIFICAITON: " + expression);
+    }
+    if (opsign == OPERATORS.mul) {
+        var stuffing = this.mul(op1, op2);
+        expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
+        console.log("SIMPLIFICAITON: " + expression);
+    }
+    if (opsign == OPERATORS.div) {
+        var stuffing = this.div(op1, op2);
+        expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
+        console.log("SIMPLIFICAITON: " + expression);
+    }
+    if (opsign == OPERATORS.exp) {
+        var stuffing = this.exp(op1, op2);
+        expression = expression.replaceBetween(leftstart, rightstart + 1, stuffing);
+        console.log("SIMPLIFICAITON: " + expression);
     }
 
-})();
+    return expression;
+}
 
 #endif /* WUMBO_H */
